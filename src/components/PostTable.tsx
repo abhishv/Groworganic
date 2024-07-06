e8d3e6c6b999e3e3307262e591248c84e94571d6
abhishv <120746681+abhishv@users.noreply.github.com>
2024-07-06 22:05:35 +0530

    Update PostTable.tsx

--- src/components/PostTable.tsx
+++ src/components/PostTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Post } from '../models/Post';
 
